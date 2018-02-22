77c132e607b31cd773769a3d572d509ceb806d40
Vidar Tonaas Fauske <vidartf@gmail.com>
2018-02-22 18:44:21 +0100

    Use newest widget-gen with template

--- src/widgets/gen.ts
+++ src/widgets/gen.ts
@@ -1,22 +1,20 @@
-
 import {
   WidgetModel, DOMWidgetModel,
   WidgetView, DOMWidgetView,
-  unpack_models
+  unpack_models, ManagerBase
 } from '@jupyter-widgets/base';
 
 import {
-  data_union_serialization
+  data_union_serialization, array_serialization
 } from 'jupyter-dataserializers';
 
 
 import {
-  ManagerBase
-} from '@jupyter-widgets/base';
-
-import {
   VtkWidgetModel
 } from './base';
+
+
+
 
 /**
  * Type declaration for general widget serializers.
@@ -35,153 +33,187 @@ export
 class DataArrayModel extends VtkWidgetModel {
 
   defaults() {
-    return {...super.defaults(), ...{
+    return {...super.defaults(), ... {
       _model_name: "DataArrayModel",
       data: undefined,
       name: null,
-    }}
+    }};
   }
 
   static serializers = {
     ...VtkWidgetModel.serializers,
-    data: data_union_serialization,
-  };
+  }
+
 }
+
 
 
 export
 class DataContainerModel extends VtkWidgetModel {
 
   defaults() {
-    return {...super.defaults(), ...{
+    return {...super.defaults(), ... {
       _model_name: "DataContainerModel",
       attributes: undefined,
       data_arrays: undefined,
       kind: undefined,
-    }}
+    }};
   }
 
   static serializers = {
     ...VtkWidgetModel.serializers,
-    data_arrays: { deserialize: unpack_models },
-  };
+  }
+
 }
+
 
 
 export
 class DataSetModel extends VtkWidgetModel {
 
   defaults() {
-    return {...super.defaults(), ...{
+    return {...super.defaults(), ... {
       containers: undefined,
-    }}
+    }};
   }
 
   static serializers = {
     ...VtkWidgetModel.serializers,
-    containers: { deserialize: unpack_models },
-  };
+  }
+
 }
+
 
 
 export
 class MutableDataSetModel extends DataSetModel {
 
   defaults() {
-    return {...super.defaults(), ...{
+    return {...super.defaults(), ... {
       _model_name: "MutableDataSetModel",
       kind: undefined,
       metadata: undefined,
-    }}
+    }};
+  }
+
+  static serializers = {
+    ...DataSetModel.serializers,
   }
 
 }
+
 
 
 export
 class ImageDataModel extends DataSetModel {
 
   defaults() {
-    return {...super.defaults(), ...{
+    return {...super.defaults(), ... {
       _model_name: "ImageDataModel",
       origin: [],
       spacing: [],
       whole_extent: [],
-    }}
+    }};
+  }
+
+  static serializers = {
+    ...DataSetModel.serializers,
   }
 
 }
+
 
 
 export
 class RectilinearGridModel extends DataSetModel {
 
   defaults() {
-    return {...super.defaults(), ...{
+    return {...super.defaults(), ... {
       _model_name: "RectilinearGridModel",
       whole_extent: [],
-    }}
+    }};
+  }
+
+  static serializers = {
+    ...DataSetModel.serializers,
   }
 
 }
+
 
 
 export
 class StructuredGridModel extends DataSetModel {
 
   defaults() {
-    return {...super.defaults(), ...{
+    return {...super.defaults(), ... {
       _model_name: "StructuredGridModel",
       whole_extent: [],
-    }}
+    }};
+  }
+
+  static serializers = {
+    ...DataSetModel.serializers,
   }
 
 }
+
 
 
 export
 class PolyDataModel extends DataSetModel {
 
   defaults() {
-    return {...super.defaults(), ...{
+    return {...super.defaults(), ... {
       _model_name: "PolyDataModel",
-    }}
+    }};
+  }
+
+  static serializers = {
+    ...DataSetModel.serializers,
   }
 
 }
+
 
 
 export
 class UnstructuredGridModel extends DataSetModel {
 
   defaults() {
-    return {...super.defaults(), ...{
+    return {...super.defaults(), ... {
       _model_name: "UnstructuredGridModel",
-    }}
+    }};
+  }
+
+  static serializers = {
+    ...DataSetModel.serializers,
   }
 
 }
+
 
 
 export
 class VtkRendererModel extends DOMWidgetModel {
 
   defaults() {
-    return {...super.defaults(), ...{
+    return {...super.defaults(), ... {
       _model_module: "jupyter-vtk-datawidgets",
-      _model_module_version: "0.1.0",
+      _model_module_version: "1.0.0",
       _model_name: "VtkRendererModel",
       _view_module: "jupyter-vtk-datawidgets",
-      _view_module_version: "0.1.0",
+      _view_module_version: "1.0.0",
       _view_name: "VtkRendererView",
       background: [0,0,0],
       dataset: undefined,
-      size: [512, 512],
-    }}
+      size: [600,400],
+    }};
   }
 
   static serializers = {
     ...DOMWidgetModel.serializers,
-    dataset: { deserialize: unpack_models },
-  };
+  }
+
 }
+
